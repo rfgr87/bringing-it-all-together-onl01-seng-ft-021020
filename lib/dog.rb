639f39dba7ef2f108ddc537cc6550d81ac5f26fc
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-03-12 17:58:27 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,7 +2,12 @@ class Dog
   attr_accessor :name, :breed 
   attr_reader :id 
   
-  def initialize(id:,name:,breed:)
-    @id = id;
+  def initialize(id:=nil,name:,breed:)
+    @id = id
+    @name = name
+    @breed = breed 
+  end
+  
+  
   
 end
